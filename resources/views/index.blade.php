8757208e798245428c3ed4da8edcd290a5af1d15
VangelisGiannakosian <vgiannakosian@pavla.gr>
2020-05-16 00:03:26 +0300

    completed validation extracted layout.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,77 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-    <title>XS Demo</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" />
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css"/>
+@endsection
 
-        .full-height {
-            height: 100vh;
-        }
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
 
-        .position-ref {
-            position: relative;
-        }
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="row">
 
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
-</head>
-<body>
-<div class="flex-center position-ref full-height">
-    <div class="row">
-
-        <h2 class="text-center w-100 mb-5">
-            XS DEMO
-        </h2>
+            <h2 class="text-center w-100 mb-5">
+                XS DEMO
+            </h2>
 
             <div class="col-sm-12">
                 <form action="{{ route('store') }}" method="POST" class="form_validate" autocomplete="off">
@@ -80,7 +23,9 @@
                         <div class="form-group col-md-6">
 
                             <label for="company_symbol">Company Symbol</label>
-                            <select name="company_symbol"  class="form-control select2 @error('company_symbol') is-invalid @enderror" required id="company_symbol">
+                            <select name="company_symbol"
+                                    class="form-control select2 @error('company_symbol') is-invalid @enderror" required
+                                    id="company_symbol">
                                 <option selected disabled value="">Choose one of the Symbols</option>
                                 @foreach($companySymbols as $symbol)
                                     <option value="{{ $symbol }}">{{$symbol}}</option>
@@ -90,22 +35,26 @@
                         </div>
                         <div class="form-group col-md-6">
                             <label for="inputEmail4">Email</label>
-                            <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" id="inputEmail4" required placeholder="Email">
+                            <input type="email" name="email" value="{{ old('email') }}"
+                                   class="form-control @error('email') is-invalid @enderror" id="inputEmail4" required>
                             <x-errors type="email"/>
                         </div>
                     </div>
 
-
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label for="s_date">Start Date</label>
-                            <input type="text" name="start_date" value="{{ old('start_date') }}" class="form-control datepicker @error('start_date') is-invalid @enderror" required id="s_date" >
+                            <label for="start_date">Start Date</label>
+                            <input type="text" name="start_date" value="{{ old('start_date') }}"
+                                   class="form-control datepicker @error('start_date') is-invalid @enderror" required
+                                   id="start_date">
                             <x-errors type="start_date"/>
                         </div>
 
                         <div class="form-group col-md-6">
-                            <label for="e_date">End Date</label>
-                            <input type="text" name="end_date" value="{{ old('end_date') }}"  class="form-control datepicker @error('end_date') is-invalid @enderror" required id="e_date" >
+                            <label for="end_date">End Date</label>
+                            <input type="text" name="end_date" value="{{ old('end_date') }}"
+                                   class="form-control datepicker @error('end_date') is-invalid @enderror" required
+                                   id="end_date">
                             <x-errors type="end_date"/>
                         </div>
                     </div>
@@ -115,56 +64,63 @@
                 </form>
 
             </div>
+        </div>
     </div>
-</div>
-<script  src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-{{--<script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>--}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/js/select2.min.js"></script>--}}
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/3.5.4/select2.min.js"></script>--}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
-<script>
+@endsection
 
-$(document).ready(function () {
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
+    <script>
 
-    $('.select2').select2();
+        $(document).ready(function () {
+
+            //Initialize select2 element
+            $('.select2').select2();
 
 
-    $('.datepicker').datepicker({
-        endDate: '+0d',
-        todayHighlight:true,
-        orientation: 'bottom',
-        format:'yyyy-mm-dd',
-    });
+            //Initialize datepicker elements
+            $('.datepicker').datepicker({
+                endDate: '+0d',
+                todayHighlight: true,
+                orientation: 'bottom',
+                format: 'yyyy-mm-dd',
+            });
 
-    $('#s_date').on('changeDate',(selected)=>{
-        const currentDate = new Date(selected.date.valueOf());
-        const endDateInput =  $('#e_date');
-
-
-        if(endDateInput.val()) {
-            const eDate = new Date(endDateInput.val());
-            if(eDate.getTime() < currentDate.getTime())
-            {
-                console.log('inside')
-                endDateInput.datepicker("update", currentDate);
-            }
-        }
-
-        endDateInput.datepicker('setStartDate', currentDate);
-
-    });
+            //Datepicker front end validation functionality
+            $('#start_date').on('changeDate', (selected) => {
+                const currentDate = new Date(selected.date.valueOf());
+                const endDateInput = $('#end_date');
+                if (endDateInput.val()) {
+                    const eDate = new Date(endDateInput.val());
+                    if (eDate.getTime() < currentDate.getTime()) {
+                        endDateInput.datepicker("update", currentDate);
+                    }
+                }
+                endDateInput.datepicker('setStartDate', currentDate);
+            });
 
 
-    $('.form_validate').validate({
-        errorPlacement: function(error, element) {
-            element.closest('.form-group').append(error)
-        }
-    });
-});
+            //Form validation
+            $('.form_validate').validate({
+                rules: {
+                    start_date: {
+                        required: true,
+                        date: true
+                    },
+                    end_date: {
+                        required: true,
+                        date: true
+                    }
+                },
+                errorPlacement: function (error, element) {
+                    element.closest('.form-group').append(error)
+                }
+            });
+        });
 
-</script>
-</body>
-</html>
+    </script>
+@endsection
+
+
